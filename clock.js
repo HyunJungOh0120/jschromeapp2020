da7a17da3a9f8764b406b809ff44d2ddb3774c08
HyunJungOh0120 <72025960+HyunJungOh0120@users.noreply.github.com>
2020-11-28 21:58:35 +0800

    finished clock, username, todolist

--- clock.js
+++ clock.js
@@ -1,11 +1,18 @@
 const clockContainer = document.querySelector(".js-clock"),
-  clockTitle = clockContainer.querySelector("h1");
+  clockDate = clockContainer.querySelector(".js-clock__date"),
+  clockTitle = clockContainer.querySelector(".js-clock__time");
 
 function getTime() {
   const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDay();
   const minutes = date.getMinutes();
   const hours = date.getHours();
   const seconds = date.getSeconds();
+
+  clockDate.innerText = `${day}.${month}.${year}`;
+
   clockTitle.innerText = `${hours < 10 ? `0${hours}` : `${hours}`}:${
     minutes < 10 ? `0${minutes}` : `${minutes}`
   }:${seconds < 10 ? `0${seconds}` : `${seconds}`}`;
